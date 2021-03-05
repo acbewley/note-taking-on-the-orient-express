10d10bf19bb086fb2a8df68508894f254cc52889
acbewley <acbewley13@gmail.com>
2021-03-05 18:29:31 -0500

    working on writing db.json

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db.json')));
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+    fs.readFile('db.json', 'utf8', (err, data) => {
+        if (err) console.error(err)
+        let notes = [];
+        for (i = 0; i < data.length; i++) {
+           notes.push(data[i]) 
+        }
+        
+        console.log(notes)
+        notes.push(newNote)
+        // console.log(notes)
+    })
+})
 
 app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
